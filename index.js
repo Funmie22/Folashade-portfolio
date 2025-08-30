8dd431a53cf44eb49db5d62207452617e05b7fd0
Dahunsi Funmilayo <funmi.425@gmail.com>
2025-08-30 02:58:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector("nav ul");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    hamburger.innerHTML = navMenu.classList.contains("active") 
+      ? '<i class="fas fa-times"></i>'  
+      : '<i class="fas fa-bars"></i>'; 
+  });
